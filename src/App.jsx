0f757f7bc7d89716148872ea020a05becdafaca5
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2025-01-31 23:04:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import React from "react";
 const App = () => {
     return (
         <div>
-            <div>
+            <div className="cont">
                 <h1>Product Listing</h1>
                 <div>
-                    <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image.jpg" />
+                    <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image.jpg"/>
+                    <p>This is a gaming console</p>
+                </div>
+                <div>
+                    <button> Add to Cart </button> <br/>
+                    <button>Remove</button>
                 </div>
             </div>
 
