008fa5b54f05fb6051ae0114a0cb2a420053dbd7
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2025-01-31 23:07:35 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const App = () => {
     return (
         <div>
-            <div className="cont">
+            <div className="w-72">
                 <h1>Product Listing</h1>
-                <div>
+                <div className="w-50">
                     <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image.jpg"/>
                     <p>This is a gaming console</p>
                 </div>
