aa775ac2b048628d6541cddba6e63790706c75c2
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2025-02-06 22:57:09 +0530

    seecond  commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import React from "react";
 const App = () => {
     return (
         <div>
-            <div className="w-72">
-                <h1>Product Listing</h1>
-                <div className="w-50">
-                    <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image.jpg"/>
-                    <p>This is a gaming console</p>
-                </div>
+            <div className="w-72 h-96 bg-gradient-to-r from-indigo-500 to-indigo-900 m-10 ">
+                <h1 className="text-center text-white text-2xl mb-3 p-6">Product Listing</h1>
+                    <img className="rounded-xl w-86 mb-3 bg-contain" src="https://images.unsplash.com/photo-1526509867162-5b0c0d1b4b33?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image.jpg"/>
+                    <p className="text-center text-white mb-3">This is a gaming console with a 4K resolution and 16GB of RAM </p>
                 <div>
                     <button> Add to Cart </button> <br/>
                     <button>Remove</button>
