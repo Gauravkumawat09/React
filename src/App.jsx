52225f7901fd4901f2d473496a3fc583ff4d70ae
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2025-02-07 15:22:40 +0530

    seecond  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
+import CardComponent from "./components/CardComponent";
+import products from "./utility/data";
 
 const App = () => {
-    return (
-        <div>
-            <div className="w-72 h-96 bg-gradient-to-r from-indigo-500 to-indigo-900 m-10 rounded-2xl">
-                <h1 className="text-center text-white text-2xl mb-3 p-6">Product Listing</h1>
-                    <img className="rounded-xl w-86 mb-3 bg-contain" src="https://images.unsplash.com/photo-1526509867162-5b0c0d1b4b33?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image.jpg"/>
-                    <p className="text-center text-white mb-3">This is a gaming console with a 4K resolution and 16GB of RAM </p>
-                <div>
-                    <button> Add to Cart </button> <br/>
-                    <button>Remove</button>
-                </div>
-            </div>
-                {/* //6 feburary */}
-        </div>
-    )
-} 
-
+  return (
+    <div className="flex flex-wrap bg-emerald-200">
+    <CardComponent contacts={products} />
+    </div>
+  );
+};
 export default App;
--- src/utility/data.js
+++ src/utility/data.js
@@ -18,28 +18,29 @@ const products = [
         name: "Smartphone", 
         price: 50000,
         productdesc: "Smartphone with 12GB RAM and 256GB Storage", 
-        image: "https://images.unsplash.com/photo-1580910051074-3eb694886505?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        image: "https://images.unsplash.com/photo-1483478550801-ceba5fe50e8e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c21hcnRwaG9uZXxlbnwwfHwwfHx8MA%3D%3D" 
     },
     {
         id: 4, 
         name: "Tablet", 
         price: 30000, 
         productdesc: "Tablet with 8GB RAM and 128GB Storage",
-        image: "https://images.unsplash.com/photo-1561154464-82e9adf32764?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        image: "https://images.unsplash.com/photo-1590103514924-009a76183beb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHRhYmxldHxlbnwwfHwwfHx8MA%3D%3D"
     },
     {
         id: 5, 
         name: "Smartwatch", 
         price: 5000, 
         productdesc: "Smartwatch with fitness tracking",
-        image: "https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        image: "https://images.unsplash.com/photo-1595590677969-becff900f5f0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c21hcnR3YXRjaHxlbnwwfHwwfHx8MA%3D%3D"
     },
     {
         id: 6, 
         name: "Gaming Console", 
         price: 50000, 
         productdesc: "Gaming Console with 4K support",
-        image: "https://images.unsplash.com/photo-1509198397868-475647b2a1e5?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        image: "https://images.unsplash.com/photo-1526509867162-5b0c0d1b4b33?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGdhbWluZyUyMGNvbnNvbGV8ZW58MHx8MHx8fDA%3D"
     }
   ];
+  export default products;
   
